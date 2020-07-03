9984369b440456f58cd261a9e1f2e264904ac813
Victor Joseph <victornjoseph@gmail.com>
2020-07-04 01:24:47 +0300

    initial commit

---
+++ flashlearn/__init__.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+
+
+def create_app(config = None):
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY = 'dummy',
+    )
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def hello():
+        return 'Hello, World!'
+
+    return app
+
