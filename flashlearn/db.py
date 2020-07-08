3ed927d775204460258949f1a69563a602eb0c75
Victor Joseph <victornjoseph@gmail.com>
2020-07-08 17:15:35 +0300

    add clear-db command for dropping tables

--- flashlearn/db.py
+++ flashlearn/db.py
@@ -32,11 +32,16 @@ def init_db():
 	Base.metadata.create_all(bind=engine)
 
 
+def clear_db():
+	import flashlearn.models
+	Base.metadata.drop_all(bind = engine)
+
+
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
 	confirm = click.prompt(
-		'The database will be recreated and all data will be lost.\
+		'The database will be re-defined.\
 		\nReply with Y/N to confirm or cancel', type=str)
 	if confirm == 'Y' or confirm == 'y':
 		init_db()
@@ -45,9 +50,24 @@ def init_db_command():
 		click.echo("Cancelled db init.")
 
 
+@click.command('clear-db')
+@with_appcontext
+def clear_db_command():
+	confirm = click.prompt(
+		'All database tables will be dropped and all data will be lost.\
+		\nReply with Y/N to confirm or cancel.', type=str)
+	if confirm == 'Y' or confirm == 'y':
+		clear_db()
+		click.echo('All database tables successfully deleted.')
+	else:
+		click.echo('Canceled clear_db.')
+
+
 def init_app(app):
-	"""Creates engine and initializes session here because of the necessity
+	"""
+	Creates engine and initializes session here because of the necessity
 	of app_context especially when creating the engine
 	"""
 	app.teardown_appcontext(close_db_session)
 	app.cli.add_command(init_db_command)
+	app.cli.add_command(clear_db_command)
