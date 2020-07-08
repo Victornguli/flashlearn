f9c167914a6a625bc03e953fb993f49fd4c5920c
Victor Joseph <victornjoseph@gmail.com>
2020-07-08 03:10:52 +0300

    add basemodel & other required models

--- flashlearn/models.py
+++ flashlearn/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from flashlearn.db import Base
@@ -20,6 +20,14 @@ class TimestampedModel(Base):
 		"""Save a user to a database. This includes creating a new user and editing too"""
 		db_session.add(self)
 		db_session.commit()
+
+
+class BaseModel(TimestampedModel):
+	"""A base model class implementing name and description fields"""
+	__abstract__ = True
+
+	name = Column(String(20), nullable = True)
+	description = Column(String(50))
 
 
 class User(TimestampedModel):
@@ -44,16 +52,16 @@ class User(TimestampedModel):
 		return f'<User: {self.username} - {self.state}>'
 
 
-class Group(TimestampedModel):
+class Group(BaseModel):
 	"""Group model class"""
 	__tablename__ = 'groups'
 
-	name = Column(String(20), nullable = False)
-	description = Column(String(20), nullable = False)
-	user_id = Column(Integer, ForeignKey('users.id'), nullable = True)
-	user = relationship('User', backref = 'users')
-	parent_id = Column(Integer, ForeignKey('groups.id'), nullable = True)
+	user_id = Column(Integer, ForeignKey('users.id'))
+	user = relationship('User', backref = 'user_groups')
+	parent_id = Column(Integer, ForeignKey('groups.id'))
 	children = relationship('Group')
+	group_study_plans = relationship(
+		'StudyPlanGroup', backref = 'group_study_plans', cascade = 'all, delete-orphan')
 
 	def __init__(self, name, description, user_id = None, parent_id = None):
 		"""Setup Group entry"""
@@ -65,3 +73,67 @@ class Group(TimestampedModel):
 	def __repr__(self):
 		children = [i.name for i in self.children] if self.children else []
 		return f'<Group: {self.name} - {children}>'
+
+
+class Card(BaseModel):
+	"""Class for Card model"""
+	__tablename__ = 'cards'
+
+	front = Column(Text(), nullable = False)
+	back = Column(Text(), nullable = False)
+	is_snippet = Column(Boolean(), default = False)
+	user_id = Column(Integer, ForeignKey('users.id'), nullable = False)
+	user = relationship('User', backref = 'user_cards')  # usable via user.user_cards
+	group_id = Column(Integer, ForeignKey('groups.id'), nullable = False)
+	group = relationship('Group', backref = 'group_cards')  # usable via group.group_cards
+
+	def __init__(
+			self, name, front, back, user_id, group_id, description = None, is_snippet = False):
+		"""Setup card instance"""
+		self.name = name
+		self.front = front
+		self.back = back
+		self.description = description
+		self.is_snippet = is_snippet
+		self.user_id = user_id
+		self.group_id = group_id
+
+	def __repr__(self):
+		return f'<Card: {self.name} - {self.user.username} - {self.group.name}>'
+
+
+class StudyPlan(BaseModel):
+	"""Study plan model class"""
+	__tablename__ = 'study_plans'
+
+	ordering = Column(String(10))
+	see_solved = Column(Boolean(), default = False)
+	user_id = Column(Integer, ForeignKey('users.id'))
+	user = relationship('User', backref = 'user_study_plans')  # backref to user -> study_plans
+	study_plan_groups = relationship(
+		'StudyPlanGroup', backref = 'study_plan_groups', cascade = 'all, delete-orphan')
+
+	def __init__(self, name, ordering = False, user_id = None):
+		"""Initialize study plan instance"""
+		self.name = name
+		self.ordering = ordering
+		self.user_id = user_id
+
+	def __repr__(self):
+		return f'<StudyPlan: {self.name} - {self.state}>'
+
+
+class StudyPlanGroup(BaseModel):
+	"""Many to Many through table for groups and study plans"""
+	__tablename__ = 'study_plan_groups'
+
+	group_id = Column(Integer, ForeignKey('groups.id'), nullable = False)
+	study_plan_id = Column(Integer, ForeignKey('study_plans.id'), nullable = False)
+
+	def __init__(self, group_id, study_plan_id):
+		"""Initialize StudyPlanGroup association"""
+		self.group_id = group_id
+		self.study_plan_id = study_plan_id
+
+	def __repr__(self):
+		return f'<StudyPlanGroup: {self.study_plan.name} - {self.group.name}>'
