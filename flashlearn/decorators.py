61ce4ab018be5df0e926bb4f5fab7a37ec55ee58
Victor Joseph <victornjoseph@gmail.com>
2020-07-10 21:29:28 +0300

    update decorator to lookup blueprints

--- flashlearn/decorators.py
+++ flashlearn/decorators.py
@@ -8,6 +8,10 @@ def login_required(view):
 		if g.user is None:
 			# Hacky way of reversing the view url then adding it to the next argument
 			# for an intuitive redirect after login. view.__name__ -> the route method name
-			return redirect(url_for('auth.login', next = url_for(view.__name__)))
+			view_url = view.__name__
+			if len(view.__module__.split(".")) > 2:
+				bp = (view.__module__.split(".")[1])  # Get the blueprint name, for accurate urls.
+				view_url = f'{bp}.{view_url}'
+			return redirect(url_for('auth.login', next = url_for(view_url)))
 		return view(**kwargs)
 	return wrapped_view
