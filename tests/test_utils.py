2bbe3b15077bfacf5e1c7cb7bd88e3ba1b549fe5
Victor Joseph <victornjoseph@gmail.com>
2020-07-17 23:21:18 +0300

    [Tests] add tests_utils.py

---
+++ tests/test_utils.py
@@ -0,0 +1,17 @@
+import pytest
+from .conftest import BaseTestCase
+from flashlearn.utils import to_bool
+
+
+class TestUtils(BaseTestCase):
+
+	def test_to_boolean(self):
+		assert to_bool("0") is False, "Should return False"
+		assert to_bool("1") is True, "Should return True"
+		assert to_bool("true") is True, "Should return True"
+		assert to_bool("false") is False, "Should return False"
+		assert to_bool(True) is True, "Should return True"
+		assert to_bool(False) is False, "Should return False"
+
+		with pytest.raises(ValueError):
+			to_bool("a shrubbery!")
