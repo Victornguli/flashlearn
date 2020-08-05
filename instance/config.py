2d10441031f796902ce71ac1ddf5a47f9532e38b
Victor Joseph <victornjoseph@gmail.com>
2020-08-05 12:57:40 +0300

    add sqlite compatibility check

--- instance/config.py
+++ instance/config.py
@@ -1,6 +1,14 @@
 import os
+import sys
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 ROOT_DIR = os.path.abspath(os.path.dirname(BASE_DIR))
+
+
+WIN = sys.platform.startswith('win')
+if WIN:
+	prefix = 'sqlite:///'
+else:
+	prefix = 'sqlite:////'
 
 
 class BaseConfig:
@@ -9,7 +17,7 @@ class BaseConfig:
 	DEBUG = False
 	CSRF_ENABLED = True
 	FLASK_APP = 'flashlearn'
-	DATABASE = os.getenv('DATABASE')
+	DATABASE_URI = os.getenv('DATABASE_URI')
 	LOG_FILE = os.getenv('LOG_FILE') or os.path.join(ROOT_DIR, 'flashlearn.log')
 	LOG_LEVEL = 20
 
@@ -18,7 +26,7 @@ class DevelopmentConfig(BaseConfig):
 	"""Development config class"""
 	DEBUG = True
 	db_path = os.path.join(BASE_DIR, 'dev_db.sqlite3')
-	DATABASE = f'sqlite:///{db_path}'
+	DATABASE_URI = f'{prefix}{db_path}'
 
 
 class TestingConfig(BaseConfig):
@@ -26,7 +34,7 @@ class TestingConfig(BaseConfig):
 	DEBUG = True
 	TESTING = True
 	# db_path = os.path.join(os.path.abspath(os.path.dirname(BASE_DIR)), 'tests/test_db.sqlite3')
-	DATABASE = 'sqlite:///:memory:'
+	DATABASE_URI = f'{prefix}:memory:'
 	# Fix to use in-memory db for tests. Replace with above db path to save test data in-file.
 
 
@@ -35,7 +43,7 @@ class ProductionConfig(BaseConfig):
 	DEBUG = False
 	TESTING = False
 	db_path = os.path.join(BASE_DIR, 'dev_db.sqlite3')
-	DATABASE = f'sqlite:///{db_path}'  # Replace with production database..
+	DATABASE_URI = f'{[prefix]}{db_path}'  # Replace with production database..
 
 
 app_config = {
