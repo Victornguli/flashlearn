5bbad8d25081bc0c0c8deb7de38f232437afda2e
Victor Joseph <victornjoseph@gmail.com>
2020-09-03 00:46:59 +0300

    add necessary pytest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,6 +27,37 @@ def user(client) -> User:
 
 
 @pytest.fixture
+def decks(client, user):
+	algos = Deck(
+		name = 'Algorithms', description = 'Common Algorithms in Computer Science', user = user)
+	algos.save()
+	dp = Deck(
+		name = 'DP', description = 'Dynamic Programming', user = user, parent_id = algos.id)
+	dp.save()
+	return algos, dp
+
+
+@pytest.fixture
+def card(client, decks, user):
+	back = """
+	Dynamic Programming (DP) is an algorithmic technique for solving an optimization problem by breaking it down
+	into simpler subproblems and utilizing the fact that the optimal solution to the overall problem depends upon
+	the optimal solution to its subproblems.
+	"""
+	card = Card(
+		front = 'What is dynamic programming', back = back, user_id = user, deck_id = decks[1].id)
+	card.save()
+	return card
+
+
+@pytest.fixture
+def plan(client, user):
+	plan = StudyPlan(name = 'Grokking Algorithms', user = user)
+	plan.save()
+	return plan
+
+
+@pytest.fixture
 def login(client, user):
 	def inner(username = None, password = None):
 		if not (username and password):
@@ -43,46 +74,3 @@ def logout(client):
 	def inner():
 		return client.post('/user/logout')
 	return inner
-
-
-	# self.algos = Deck(
-	# 	name = 'Algorithms', description = 'Common Algorithms in Computer Science', user = self.alice)
-	# self.algos.save()
-	# self.dp = Deck(
-	# 	name = 'DP', description = 'Dynamic Programming', user = self.alice, parent_id = self.algos.id)
-	# self.dp.save()
-	#
-	# back = """
-	# Dynamic Programming (DP) is an algorithmic technique for solving an optimization problem by breaking it down
-	# into simpler subproblems and utilizing the fact that the optimal solution to the overall problem depends upon
-	# the optimal solution to its subproblems.
-	# """
-	# self.card = Card(
-	# 	front = 'What is dynamic programming', back = back, user_id = self.alice.id, deck_id = self.dp.id)
-	# self.card.save()
-	#
-	# self.plan = StudyPlan(name = 'Grokking Algorithms', user = self.alice)
-	# self.plan.save()
-
-# class BaseTestCase:
-# 	"""Base test case"""
-# 	def __init__(self, client) -> None:
-# 		# Setup common model instances
-#
-#
-# 	def login(self, username = None, password = None):
-# 		if not (username and password):
-# 			username = self.alice.username
-# 			password = 'password'
-# 		return client.post(
-# 			'/user/login', data = dict(username = username, password = password),
-# 			follow_redirects = True)
-#
-# 	def logout(self):
-# 		return client.post('/user/logout')
-
-	# @staticmethod
-	# def refresh(*args):
-	# 	"""Refresh objects as needed in derived test cases"""
-	# 	for obj in args:
-	# 		db.session.refresh(obj)
