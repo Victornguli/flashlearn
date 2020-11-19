5066ee2ffea87720f8d2f733b419198b0055d0d9
Victor Joseph <victornjoseph@gmail.com>
2020-11-19 14:39:12 +0300

    add alembic migration
    register db migration command via click

--- flashlearn/commands.py
+++ flashlearn/commands.py
@@ -1,4 +1,5 @@
 import click
+from flask_migrate import Migrate, MigrateCommand
 from flask.cli import with_appcontext
 from flashlearn import db
 
@@ -6,8 +7,11 @@ from flashlearn import db
 @click.command("init-db")
 @with_appcontext
 def init_db_command():
+    """
+    Initialize a local database on devevelopment
+    """
     confirm = click.prompt(
-        "The database will be re-defined." "\nReply with Y/N to confirm or cancel",
+        "The database will be re-initialized." "\nReply with Y/N to confirm or cancel",
         type=str,
     )
     if confirm.lower() == "y":
@@ -17,51 +21,55 @@ def init_db_command():
         click.echo("Cancelled db init.")
 
 
-@click.command("clear-db")
+@click.command("drop-all")
 @with_appcontext
-def clear_db_command():
+def drop_all_command():
+    """
+    Drop all tables on current env
+    Don't use this command on production
+    """
     confirm = click.prompt(
-        "All database tables will be dropped and all data will be lost."
-        "\nReply with Y/N to confirm or cancel.",
+        "All database tables will be dropped." "\nReply with Y/N to confirm or cancel.",
         type=str,
     )
     if confirm.lower() == "y":
         db.drop_all()
         click.echo("All database tables successfully deleted.")
     else:
-        click.echo("Cancelled clear-db.")
-
-
-def prompt_password():
-    password = click.prompt("Password", type=str, hide_input=True)
-    password_2 = click.prompt("Confirm your password", type=str, hide_input=True)
-    if password == password_2:
-        return password
-    else:
-        click.echo("Passwords do not match. Try again.")
-        prompt_password()
+        click.echo("Cancelled drop-all.")
 
 
 @click.command("create-user")
 @with_appcontext
 def create_user_command():
+    """Quick create a user for development env"""
+
     from flashlearn.models import User
 
     username = click.prompt("Username", type=str)
     if User.query.filter_by(username=username).first() is None:
-        password = prompt_password()
+        password = click.prompt(
+            "Password",
+            type=str,
+            hide_input=True,
+            confirmation_prompt=True,
+        )
         email = click.prompt("Email", default=None)
         u = User(username=username, password=password, email=email)
         u.save()
         click.echo("User created successfully.")
     else:
-        click.echo("Username already in use. Try again with a different one.")
+        click.echo("Username not available. Try again with a different one.")
 
 
 def register_commands(app):
     """
-    Registers custom CLI commands via click.
+    Registers custom CLI commands via click withing the app context..
     """
+    # Setup Alembic Migrations
+    Migrate(app, db)
+    app.cli.add_command(MigrateCommand, name="db")
+
     app.cli.add_command(init_db_command)
-    app.cli.add_command(clear_db_command)
+    app.cli.add_command(drop_all_command)
     app.cli.add_command(create_user_command)
