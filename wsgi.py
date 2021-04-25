894ae81d3d1e2937d2f1c1ce1da63419badf73b2
Victor Nguli <victornjoseph@gmail.com>
2021-04-26 02:53:00 +0300

    Add nginx dockerfile and config for a reverse proxy

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,9 @@
+import sys
+import logging
 from flashlearn import create_app
 
+logging.basicConfig(stream=sys.stderr)
+sys.path.insert(0, "/var/www/")
 app = create_app()
 
 if __name__ == "__main__":
